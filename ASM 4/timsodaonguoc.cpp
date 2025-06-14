332e7aaaf2713153c3ea9eb5b89eb81e2a7d538c
Ketam1ng <mingduu05@gmail.com>
2025-06-14 10:13:37 +0700

    Add files via upload
    up lại ASM4

--- ASM 4/timsodaonguoc.cpp
+++ ASM 4/timsodaonguoc.cpp
@@ -5,6 +5,15 @@ int main(){
 	printf("Nhap so can dao nguoc: ");
 	scanf("%d",&n);
 	
+	int m = 0;
 	
+	while(n!=0){
+		int l;
+		l=n%10;
+		m=m*10+l;
+		n=n/10;
+	}
+	printf("so dao nguoc la: %d",m);
+	return 0;
 	
 }
